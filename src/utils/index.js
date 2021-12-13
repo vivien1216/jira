2013c4a50949858f571349b01c09a6637c7dd524
vivien1216 <[Email@example.com](mailto:1870495688@qq.com)>
2021-12-13 20:21:19 +0800

    feat: 添加工程列表

---
+++ src/utils/index.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: vivien
+ * @Date: 2021-12-13 19:31:58
+ * @Last Modified by: vivien
+ * @LastEditTime: 2021-12-13 20:13:12
+ */
+export const isFalsy = (value) => (value === 0 ? false : !value);
+
+// 在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object) => {
+  // Object.assign({}, object)
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    // 0
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
