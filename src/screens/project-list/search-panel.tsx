b2b693461f2c27e558c76ab04066d87a036a905a
vivien1216 <[Email@example.com](mailto:1870495688@qq.com)>
2022-04-03 15:51:24 +0800

    fix: 添加ts类型

---
+++ src/screens/project-list/search-panel.tsx
@@ -0,0 +1,59 @@
+/*
+ * @Author: vivien
+ * @Date: 2021-12-10 09:25:15
+ * @Last Modified by: vivien
+ * @LastEditTime: 2022-04-03 15:38:03
+ */
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+}
+
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps["param"]) => void;
+}
+
+export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
+  return (
+    <form>
+      <div>
+        {/*setParam(Object.assign({}, param, {name:evt.target.value}))*/}
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            })
+          }
+        >
+          <option value={""}>负责人</option>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,33 +2,35 @@
  * @Author: vivien
  * @Date: 2021-12-13 19:31:58
  * @Last Modified by: vivien
- * @LastEditTime: 2022-02-20 16:48:22
+ * @LastEditTime: 2022-04-03 15:49:22
  */
 import { useEffect, useState } from "react";
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 // 在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   // Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
     // 0
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
     // eslint-disable-next-line
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
