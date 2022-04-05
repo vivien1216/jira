26e502381c517b935cc40197859186c82d840861
vivien1216 <[Email@example.com](mailto:1870495688@qq.com)>
2022-04-05 16:58:09 +0800

    feat: 添加登陆表单

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@
  * @Author: vivien
  * @Date: 2021-12-08 19:36:05
  * @Last Modified by: vivien
- * @LastEditTime: 2022-04-03 16:37:05
+ * @LastEditTime: 2022-04-05 16:43:18
  */
 import "./App.css";
 import { ProjectListScreen } from "screens/project-list";
-import { TsReactTest } from "try-use-array";
+// import { TsReactTest } from "try-use-array";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
       <ProjectListScreen />
-      <TsReactTest />
+      <LoginScreen />
     </div>
   );
 }
