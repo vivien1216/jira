3185f755bb9eb174b639f0a714ad36ca49354c15
vivien1216 <[Email@example.com](mailto:1870495688@qq.com)>
2022-05-04 17:45:10 +0800

    chore: 类型别名

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,7 +2,7 @@
  * @Author: vivien
  * @Date: 2022-04-05 22:14:13
  * @Last Modified by: vivien
- * @LastEditTime: 2022-04-05 22:16:01
+ * @LastEditTime: 2022-05-04 17:44:40
  */
 import qs from "qs";
 import * as auth from "auth-provider";
@@ -54,7 +54,44 @@ export const http = async (
 
 export const useHttp = () => {
   const { user } = useAuth();
-  // TODO 讲解 TS 操作符
+  // utility type 的用法：用泛型给它传入一个其他类型，然后utility type对这个类型进行某种操作
   return (...[endpoint, config]: Parameters<typeof http>) =>
     http(endpoint, { ...config, token: user?.token });
 };
+
+// // 类型别名、Utility Type 讲解
+// // 联合类型
+// let mayFavoriteNumber: string | number;
+// mayFavoriteNumber = 'seven';
+// mayFavoriteNumber = 7;
+
+// let jackFavoriteNumber: string | number;
+
+// // 类型别名在很多情况下可以和interface互换
+// // interface Person {
+// //     name: string
+// // }
+
+// // type Person = {name: string}
+// // const xiaoming:Person = {name: "xiaoming"}
+
+// // 类型别名，interface在这种情况下无法替代
+// type FavoriteNumber = string | number;
+// let roseFavoriteNumber:FavoriteNumber = 6;
+
+// // interface也没法实现Utility type
+// type Person = {
+//     name: string;
+//     age: number;
+// }
+
+// const xiaoming:Partial<Person> = {};
+// const shenMiRen:Omit<Person, 'name' | 'age'> = {}
+// type PersonKeys = keyof Person;
+// type PersonOnlyName = Pick<Person, "name" | "age">
+// type Age  = Exclude<PersonKeys, "name">
+
+// // Parcital的实现
+// type Partical<T> = {
+//     [P in keyof T]?: T[P]
+// }
