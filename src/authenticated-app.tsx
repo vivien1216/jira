3879cadb21ac12a31636dd0223d2fa4570f03059
vivien1216 <[Email@example.com](mailto:1870495688@qq.com)>
2022-05-08 17:43:48 +0800

    feat: 用flex和grid布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,18 +2,72 @@
  * @Author: vivien
  * @Date: 2022-04-05 20:49:47
  * @Last Modified by: vivien
- * @LastEditTime: 2022-05-04 23:07:36
+ * @LastEditTime: 2022-05-08 17:41:45
  */
 import { ProjectListScreen } from "screens/project-list";
 import { useAuth } from "screens/context/auth-context";
 import React from "react";
+import styled from "@emotion/styled";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}></button>
+        </HeaderRight>
+      </Header>
+      <Nav>nav</Nav>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+      <Aside>aside</Aside>
+      <Footer>footer</Footer>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas:
+    "header header header"
+    "nav main aside"
+    "footer footer footer";
+  height: 100vh;
+  grid-gap: 10rem;
+`;
+// grid-area用来给grid子元素起名字
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  grid-area: main;
+`;
+const Nav = styled.nav`
+  grid-area: nav;
+`;
+const Aside = styled.aside`
+  grid-area: aside;
+`;
+const Footer = styled.footer`
+  grid-area: footer;
+`;
